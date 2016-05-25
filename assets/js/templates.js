6821579e511b6d213d51d8ac436669a53c3c9a22
dcfvg <qwsx@dcfvg.com>
2016-05-25 13:16:00 +0200

    stacking

---
+++ assets/js/templates.js
@@ -0,0 +1,4 @@
+this["plateforme"] = this["plateforme"] || {};
+this["plateforme"]["test"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "\n\n<div>\n</div>";
+},"useData":true});
